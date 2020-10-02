7f5a85c61474e7710d586c693ec992eaa96774cb
Cesar Ramirez <LS19070100@jerez.tecnm.mx>
2020-10-02 11:25:21 -0500

    Se agregaron los geters y seters y las variables de la clase principal

--- src/PruebaMemoria.java
+++ src/PruebaMemoria.java
@@ -1,8 +1,51 @@
 
+class Alumno{
+	private int folio;
+	private String nombre;
+	private byte edad;
+	private String rs[];
+	
+	public Alumno(int folio, String nombre, byte edad, String[] rs) {
+		this.folio = folio;
+		this.nombre = nombre;
+		this.edad = edad;
+		this.rs = rs;
+	}
+	
+	
+	public int getFolio() {
+		return folio;
+	}
+	public void setFolio(int folio) {
+		this.folio = folio;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public byte getEdad() {
+		return edad;
+	}
+	public void setEdad(byte edad) {
+		this.edad = edad;
+	}
+	public String[] getRs() {
+		return rs;
+	}
+	public void setRs(String[] rs) {
+		this.rs = rs;
+	}
+
+
+
+
+
 public class PruebaMemoria {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
